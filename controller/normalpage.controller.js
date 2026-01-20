20f3c9e4861e704e4c75df069849188aaf1e9337
Nimratjotsingh <nimratjotsingh05@gmail.com>
2026-01-20 22:46:59 +0530

    howitworks page added

--- controller/normalpage.controller.js
+++ controller/normalpage.controller.js
@@ -1,3 +1,7 @@
 exports.homepage = (req, res) => {
     res.render('home.ejs');
 }
+
+exports.howItWorks= (req,res)=>{
+    res.render('howitworks.ejs');
+}
--- routes/normalpage.routes.js
+++ routes/normalpage.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const normalpage = require('../controller/normalpage.controller');
 
 router.get('/', normalpage.homepage);
+router.get('/howitworks', normalpage.howItWorks);
 
 module.exports = router;
